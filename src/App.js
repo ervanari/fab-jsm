6e00e809ff2b1d14a0f3421d7416ea91d3b71752
ervan <ervanari88@gmail.com>
2024-12-20 10:54:28 +0700

    update icon & url

--- src/App.js
+++ src/App.js
@@ -1,11 +1,12 @@
-import { FaWhatsapp, FaInstagram, FaFacebookMessenger, FaRocketchat } from "react-icons/fa";
 import Fab from "./components/floating-button-action/FloatingButtonAction.jsx";
 
 const actions = [
-  { label: "WhatsApp", icon: <FaWhatsapp />, onClick: console.log },
-  { label: "Instagram", icon: <FaInstagram />, onClick: console.log },
-  { label: "Facebook Messenger", icon: <FaFacebookMessenger />, onClick: console.log },
-  { label: "Live Chat", icon: <FaRocketchat />, onClick: console.log },
+  { label: "WhatsApp", icon: "https://img.icons8.com/3d-fluency/94/whatsapp.png", onClick: 'https://api.whatsapp.com/send?phone=628973856130' },
+  // { label: "Instagram", icon: "https://img.icons8.com/3d-fluency/94/instagram-new.png", onClick: console.log },
+  // { label: "Facebook Messenger", icon: "https://img.icons8.com/3d-fluency/94/facebook-messenger.png", onClick: console.log },
+  { label: "Chat Bot", icon: "https://img.icons8.com/3d-fluency/94/communication.png", onClick: '#' },
+  { label: "Voice Bot", icon: "https://img.icons8.com/3d-fluency/94/chatbot.png", onClick: 'https://108.143.51.70/app' },
+  { label: "Video Call", icon: "https://img.icons8.com/3d-fluency/94/video-message.png", onClick: 'https://vcall-chat.mala-widiyanto.workers.dev/set-username' },
 ];
 
 const App = () => {
--- src/components/floating-button-action/FloatingButtonAction.jsx
+++ src/components/floating-button-action/FloatingButtonAction.jsx
@@ -1,10 +1,13 @@
 import { useState } from "react";
 import cn from "classnames";
-import { MdSupportAgent } from "react-icons/md";
 import "./styles.scss";
 
 const FloatingButtonAction = ({ actions }) => {
     const [open, setOpen] = useState(false);
+
+    const handleClickUrl = (url) => {
+        window.open(url, "_blank");
+    };
 
     return (
         <ul
@@ -12,16 +15,16 @@ const FloatingButtonAction = ({ actions }) => {
             onClick={() => setOpen(!open)}
         >
             <li className="fab-button">
-                <MdSupportAgent />
+                <img src="https://img.icons8.com/3d-fluency/94/online-support--v3.png" alt="Live Chat" width={30} />
             </li>
             {actions.map((action, index) => (
                 <li
                     style={{ transitionDelay: `${index * 25}ms` }}
                     className={cn("fab-action", { open })}
                     key={action.label}
-                    onClick={action.onClick}
+                    onClick={() => handleClickUrl(action.onClick)}
                 >
-                       {action.icon}
+                       <img src={action.icon} alt={action.label} width={30} />
                     <span className="tooltip">{action.label}</span>
                 </li>
             ))}
